dff0e4c9d6ba75a4f813a1026a1e30247656e3df
majsy <madeleineauvinen@gmail.com>
2019-02-26 16:16:14 -0500

    add background gradient change img layout

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-
 require('normalize.css/normalize.css');
 require('./styles/index.scss');
 
-document.addEventListener("DOMContentLoaded", () => {
 
-    const pluginsTriggerElement = document.getElementById('plugins-trigger');
-    const pluginsElement = document.getElementById('plugins');
+const apiUrl = 'https://random.dog/woof.json'
+const section = document.querySelector('section')
+const imageContainer = document.querySelector('.img-container')
+const innerContainer = document.querySelector('.inner-container')
+const button = document.querySelector('.button-shuffle')
 
-    const pluginsVisibleClass = "splash-overview-plugins__list--visible";
+const getDog = () => {
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => { 
+      const includesMp4 = ~data.url.indexOf('mp4')
+      const includesOgg = ~data.url.indexOf('ogg')
+      const includesWebm = ~data.url.indexOf('webm')
 
-    pluginsTriggerElement.onclick = () => {
-        pluginsElement.classList.toggle(pluginsVisibleClass);
-    }
-});
+    	if (includesMp4||includesOgg||includesWebm) {
+        getDog()
+      }
+      
+      innerContainer.style.backgroundImage = `url(${data.url})`
+      // imageContainer.classList.add('fade-in')
+    })
+}
+
+const getRandomHex = () => {
+  const randomHex = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
+
+  section.style.backgroundImage = `linear-gradient(to bottom, ${randomHex}, #ffffff)`
+  button.style.color = randomHex
+}
+
+button.addEventListener('click', () => {
+  // imageContainer.classList.remove('fade-in')
+  getDog()
+  getRandomHex()
+})
+
+
